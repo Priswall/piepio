d50814622979a9c0d6bfddf52a8d5bc5b5bed948
Priswall <31701242+Priswall@users.noreply.github.com>
2019-01-18 15:11:34 -0500

    Update Barrel.js

--- scripts/Barrel.js
+++ scripts/Barrel.js
@@ -2,7 +2,7 @@ function Barrel(offsetX, offsetY, id, w, h, r) {
     this.offset = new Vector(offsetX, offsetY);
     this.id = id;
     this.w = w;
-    this.h = h;
+    this.h = -h;
     this.cooldown = 100;
     this.r = r;
     
@@ -14,7 +14,17 @@ function Barrel(offsetX, offsetY, id, w, h, r) {
         c.rotate(this.r * (Math.PI / 180));
         switch(this.id) {
             case 0:
-                rect(-this.w / 2, 0, this.w, (-this.h * 0.8) - ((this.h * 0.2) * Math.abs(Math.sin((this.cooldown / 50) - 0.5))));
+                rect(-this.w / 2, 0, this.w, (this.h * 0.8) + ((this.h * 0.2) * Math.abs(Math.sin((this.cooldown / 50) - 0.5))));
+                break;
+                
+            case 1:
+                c.beginPath();
+                c.moveTo(-this.w / 4, 0);
+                c.lineTo(-this.w / 2, (this.h * 0.8) + ((this.h * 0.2) * Math.abs(Math.sin((this.cooldown / 50) - 0.5))));
+                c.lineTo(this.w / 2, (this.h * 0.8) + ((this.h * 0.2) * Math.abs(Math.sin((this.cooldown / 50) - 0.5))));
+                c.lineTo(this.w / 4, 0);
+                c.fill();
+                c.stroke();
                 break;
         }
         c.restore();
