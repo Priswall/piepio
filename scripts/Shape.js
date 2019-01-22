321ac5b29dd49acb6407ad57d983534da5ade6c7
Priswall <31701242+Priswall@users.noreply.github.com>
2019-01-22 12:51:53 -0500

    Update Shape.js

--- scripts/Shape.js
+++ scripts/Shape.js
@@ -82,8 +82,8 @@ function Shape(x, y, id) {
     this.draw = function() {
         //Create new canvas so transparency doesn't look weird
         var tempCanvas = document.createElement("canvas");
-        this.tempCanvas.width = this.size + 2;
-        this.tempCanvas.height = this.size + 2;
+        tempCanvas.width = this.size + 2;
+        tempCanvas.height = this.size + 2;
         var tempC = tempCanvas.getContext("2d");
         tempC.lineWidth = 3;
         switch(this.id) {
