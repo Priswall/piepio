595fe182735028f3e6d5f403f7cd8ddcd193d34a
Priswall <31701242+Priswall@users.noreply.github.com>
2019-01-24 12:22:48 -0500

    Update TankStats.js

--- scripts/TankStats.js
+++ scripts/TankStats.js
@@ -2,27 +2,38 @@ var tankstats = {
     "Basic Tank": {
         reload: 500,
         bulletSpeed: 5,
-        spread: 3
+        spread: 3,
+        damage = 1
     },
     "Twin": {
         reload: 400,
         bulletSpeed: 5,
-        spread: 4
+        spread: 4,
+        damage = 1.5
     },
     "Sniper": {
         reload: 1000,
         bulletSpeed: 10,
-        spread: 1
+        spread: 1,
+        damage = 2
     },
     "Machine Gunner": {
         reload: 200,
         bulletSpeed: 5,
-        spread: 10
+        spread: 10,
+        damage = 1
     },
     "Flank Guard": {
         reload: 400,
         bulletSpeed: 5,
-        spread: 3
+        spread: 3,
+        damage = 1
+    },
+    "Destroyer": {
+        reload: 1000,
+        bulletSpeed: 4,
+        spread: 1,
+        damage = 10
     }
 };
 
